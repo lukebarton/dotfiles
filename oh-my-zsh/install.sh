f6a7e0b60e8d7d194937499403c10c41e6050b49
Luke Barton <luke@lukebarton.co.uk>
2013-09-30 13:18:37 +0100

    Removed the need to symlink oh-my-zsh by setting the config path

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -3,12 +3,3 @@ if [[ ! -a ~/.oh-my-zsh ]]
 then
   git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
-
-OHMYDIR=~/.oh-my-zsh
-if [[ -h $OHMYDIR/custom ]]
-then
-  rm -r $OHMYDIR/custom
-else
-  mv $OHMYDIR/custom $OHMYDIR/custom.backup
-fi
-ln -s "`pwd`/oh-my-zsh/custom" "$OHMYDIR"
